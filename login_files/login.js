c5b78bc8a61f771e06811c67da1f4f3d14a397e2
xinjiawei <xinjiawei@outlook.com>
2022-10-25 19:08:42 +0800

    06

--- login_files/login.js
+++ login_files/login.js
@@ -2,23 +2,38 @@
 function loginS(){
     username = document.getElementById("username").value;
     console.log(username);
-    document.cookie="username=" + username;
-    window.alert(username + " login success");
-    //document.cookie = cname + "=" + cvalue + "; " + expires;
-    window.location.replace("/html/chat_room/thread.html");
+    var settings = {
+      "url": "http://localhost:7777/api/users",
+      "method": "GET",
+      "timeout": 0,
+    };
+
+    var isvalid = 0;
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      for(p in response){
+        if(response[p].username == username) {
+          isvalid = 1;
+          break;
+        } else continue;
+      }
+      if(isvalid){
+          document.cookie="username=" + username;
+          window.alert(username + " login success");
+          //document.cookie = cname + "=" + cvalue + "; " + expires;
+          window.location.replace("/html/chat_room/thread.html");
+          console.log(isvalid);
+      } else {
+          window.alert(username + " not valid");
+          console.log(isvalid);
+      }
+    });
+
+
+
+
     }
 
-//check if logined, can justify whether logind
-function getCookie(cname){
-  var name = cname + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0; i<ca.length; i++) 
-  {
-    var c = ca[i].trim();
-    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
-  }
-  return "";
-}
 
 document.getElementById('login').addEventListener('click', loginS);
 
